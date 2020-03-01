e7d4b93e8e2c7202e560924baa590ac81eef3250
yoonjoo16 <yoonjoo16@gmail.com>
2020-03-01 12:43:38 +0100

    Fixed gui-regstrering

--- src/main/java/view/Register.java
+++ src/main/java/view/Register.java
@@ -1,6 +1,5 @@
 package view;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,61 +9,80 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+@SuppressWarnings("checkstyle:Indentation")
 public class Register extends Application {
-        private Scene scene;
-        private Button regButton;
-        private TextField nbrField;
-        private VBox vbox;
-        private HBox hbox, hbox2;
-        private ComboBox selectMonth, selectYear;
+    private ComboBox selectMonth, selectYear;
+    private ToggleGroup radioButtonGroup;
+    private RadioButton erik, yoonjoo;
+    private TextField date = new TextField();
+    private TextField place = new TextField();
+    private Button regButton = new Button("Regster");
 
-        @Override
-        public void start(Stage stage) {
-            stage.setTitle("Registrering");
-            Scene scene = new Scene(new Group(), 450, 250);
-
-            selectMonth = new ComboBox();
-            selectMonth.getItems().addAll(
-                    "januari",
-                    "februari",
-                    "mars",
-                    "april",
-                    "maj",
-                    "juni",
-                    "juli",
-                    "augusti",
-                    "september",
-                    "oktober",
-                    "november",
-                    "december"
-            );
-            selectMonth.setValue("januari");
-
-            selectYear = new ComboBox();
-            selectYear.getItems().addAll(
-                    "2020",
-                    "2019",
-                    "2018",
-                    "2017",
-                    "2016"
-            );
-            selectYear.setValue("2020");
-
-            GridPane grid = new GridPane();
-            grid.setVgap(4);
-            grid.setHgap(10);
-            grid.setPadding(new Insets(5, 5, 5, 5));
-            grid.add(new Label("Month: "), 0, 0);
-            grid.add(selectMonth, 1, 0);
-            grid.add(new Label("Year: "), 2, 0);
-            grid.add(selectYear, 3, 0);
-            grid.add(new Label("Subject: "), 0, 1);
-            grid.add(new Label("Subject: "), 0, 1);
-
-            Group root = (Group)scene.getRoot();
-            root.getChildren().add(grid);
-            stage.setScene(scene);
-            stage.show();
-
-        }
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Registrering");
+        Scene scene = new Scene(new Group(), 550, 200);
+        makeSelectBoxMonthAndYear();
+        makeRadioButtons();
+        GridPane grid = new GridPane();
+        grid.setVgap(4);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(5, 5, 5, 5));
+        grid.add(new Label("Month: "), 0, 0);
+        grid.add(selectMonth, 1, 0);
+        grid.add(new Label("Year: "), 2, 0);
+        grid.add(selectYear, 3, 0);
+        grid.add(new Label("Vem har betalat? "), 0, 1);
+        grid.add(erik, 1,2);
+        grid.add(yoonjoo,2,2);
+        grid.add(new Label("Datum (YYMMDD)"), 0, 3);
+        grid.add(date,1,3);
+        grid.add(new Label("Plats"), 0, 4);
+        grid.add(place,1,4);
+        grid.add(regButton,1,7);
+        Group root = (Group)scene.getRoot();
+        root.getChildren().add(grid);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    private void makeSelectBoxMonthAndYear() {
+        selectMonth = new ComboBox();
+        selectMonth.getItems().addAll(
+                "januari",
+                "februari",
+                "mars",
+                "april",
+                "maj",
+                "juni",
+                "juli",
+                "augusti",
+                "september",
+                "oktober",
+                "november",
+                "december"
+        );
+        selectMonth.setValue("januari");
+
+        selectYear = new ComboBox();
+        selectYear.getItems().addAll(
+                "2020",
+                "2019",
+                "2018",
+                "2017",
+                "2016"
+        );
+        selectYear.setValue("2020");
+    }
+
+    private void makeRadioButtons() {
+        radioButtonGroup = new ToggleGroup();
+
+        erik = new RadioButton("Erik");
+        erik.setToggleGroup(radioButtonGroup);
+        erik.setSelected(true);
+
+        yoonjoo = new RadioButton("Yoonjoo");
+        yoonjoo.setToggleGroup(radioButtonGroup);
+    }
+}
